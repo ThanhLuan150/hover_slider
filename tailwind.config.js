825ae43b37361a3a620963792525f1caeab9f2f5
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-07-14 16:48:27 +0700

    add

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    container:{
+      center:true
+    },
+    fontFamily:{
+      body:"'Poppins', sans-serif"
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
